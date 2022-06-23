5eff5d7514d1f5140f1405f1c6f8ff6bac81e54e
Brendan Angelo <brendanangelo12@gmail.com>
2022-06-22 17:10:30 -0700

    model files and outline text/wireframe

--- Campfire_Stories/src/main/java/com/codingdojo/campfireStories/models/Story.java
+++ Campfire_Stories/src/main/java/com/codingdojo/campfireStories/models/Story.java
@@ -1,5 +1,99 @@
 package com.codingdojo.campfireStories.models;
 
-public class Story {
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="stories")
+public class Story {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotEmpty(message="You must give your story a title!")
+	@Size(min=5,message="Your story title must be at least 5 characters long!")
+	private String storyTitle;
+	
+	@NotEmpty(message="You must have some content for your story!")
+	@Size(min=15,message="Your story must be at least 15 characters long!")
+	private String storyContent;
+	
+	@NotEmpty(message="You must give your story a genre!")
+	private String storyGenre;
+	
+	// boilerplate for story mapping
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="user_id")
+	private User user;
+	
+	// boilerplate for favorite mapping
+	@ManyToMany(fetch=FetchType.EAGER)
+	@JoinTable(
+			name="favorites",
+			joinColumns=@JoinColumn(name="favorite_id"),
+			inverseJoinColumns=@JoinColumn(name="user_id"))
+	private List<User> favoriters;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getStoryTitle() {
+		return storyTitle;
+	}
+
+	public void setStoryTitle(String storyTitle) {
+		this.storyTitle = storyTitle;
+	}
+
+	public String getStoryContent() {
+		return storyContent;
+	}
+
+	public void setStoryContent(String storyContent) {
+		this.storyContent = storyContent;
+	}
+
+	public String getStoryGenre() {
+		return storyGenre;
+	}
+
+	public void setStoryGenre(String storyGenre) {
+		this.storyGenre = storyGenre;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public List<User> getFavoriters() {
+		return favoriters;
+	}
+
+	public void setFavoriters(List<User> favoriters) {
+		this.favoriters = favoriters;
+	}
+	
+	
 }
--- Campfire_Stories/src/main/java/com/codingdojo/campfireStories/models/User.java
+++ Campfire_Stories/src/main/java/com/codingdojo/campfireStories/models/User.java
@@ -1,5 +1,113 @@
 package com.codingdojo.campfireStories.models;
 
-public class User {
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotEmpty(message="A User Name is required for registration!")
+	@Size(min=1,message="Your User Name must be at least 1 character in length.")
+	private String userName;
+	
+	@NotEmpty(message="An email is required for registration!")
+	@Email(message="Please enter a valid email address.")
+	private String email;
+	
+	@NotEmpty(message="A password is required!")
+	@Size(min=8,max=128,message="The password must have at least 8 characters!")
+	private String password;
+	
+	@Transient
+	@NotEmpty(message="A confirmation password is required!")
+	@Size(min=8,max=128,message="The confirmation password must have at least 8 characters!")
+	private String confirm;
+	
+	// boilerplate for story mapping
+	@OneToMany(mappedBy="user", fetch=FetchType.EAGER)
+	private List<Story> stories;
+	
+	// boilerplate for favorite mapping
+	@ManyToMany(fetch=FetchType.EAGER)
+	@JoinTable(
+			name="favorites",
+			joinColumns=@JoinColumn(name="user_id"),
+			inverseJoinColumns=@JoinColumn(name="favorite_id"))
+	private List<Story> storyFavorited;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirm() {
+		return confirm;
+	}
+
+	public void setConfirm(String confirm) {
+		this.confirm = confirm;
+	}
+
+	public List<Story> getStories() {
+		return stories;
+	}
+
+	public void setStories(List<Story> stories) {
+		this.stories = stories;
+	}
+
+	public List<Story> getStoryFavorited() {
+		return storyFavorited;
+	}
+
+	public void setStoryFavorited(List<Story> storyFavorited) {
+		this.storyFavorited = storyFavorited;
+	}
+	
 }
