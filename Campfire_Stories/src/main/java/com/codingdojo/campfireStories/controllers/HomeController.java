8f3bd9ce8f00f5b89b2824d5924f5feae9420c06
Carlos <acosta_carlosg@yahoo.com>
2022-06-27 22:05:56 -0600

    Reg/Log Page
    Registration and Logging is now working

--- Campfire_Stories/src/main/java/com/codingdojo/campfireStories/controllers/HomeController.java
+++ Campfire_Stories/src/main/java/com/codingdojo/campfireStories/controllers/HomeController.java
@@ -1,5 +1,77 @@
 package com.codingdojo.campfireStories.controllers;
 
-public class HomeController {
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.codingdojo.campfireStories.models.LoginUser;
+import com.codingdojo.campfireStories.models.User;
+import com.codingdojo.campfireStories.services.UserService;
+
+@Controller
+
+public class HomeController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/")
+	public String index(@ModelAttribute("newUser") User user, @ModelAttribute ("newLogin") LoginUser loginUser) {
+		return "loginRegister.jsp";
+	}
+
+	// Register User
+	
+		@PostMapping("/register")
+		public String registerUser(@Valid @ModelAttribute("newUser") User user,
+				BindingResult results,
+				HttpSession session,
+				@ModelAttribute("newLogin") LoginUser loginUser) {
+			//Validation
+			userService.validate(user, results);
+			if(results.hasErrors()) {
+				return "loginRegister.jsp";
+			}
+			
+			//Register User
+			userService.registerUser(user);
+			
+			//Put user in session
+			session.setAttribute("loggedInUser", user);
+			return "redirect:/home"; // Name of mainpage??
+		}
+		
+		
+		//Login User
+		
+		@PostMapping("/login")
+		public String loginUser(@Valid @ModelAttribute("newLogin") LoginUser loginUser,
+				BindingResult results,
+				HttpSession session,
+				@ModelAttribute("newUser") User user) {
+			
+			//Authenticate
+			userService.authenticate(loginUser, results);
+			if(results.hasErrors()) {
+				return "loginRegister.jsp";
+			}
+			User loggedInUser = userService.findByEmail(loginUser.getEmail());
+			//Put user in Session
+			session.setAttribute("LoggedInUser", loggedInUser);
+			return "redirect:/home";
+		}
+		
+		
+		//Logout
+		@GetMapping("/logout")
+		public String logout(HttpSession session) {
+			session.invalidate();
+			return "redirect:/";
+		}
 }
--- Campfire_Stories/src/main/java/com/codingdojo/campfireStories/services/UserService.java
+++ Campfire_Stories/src/main/java/com/codingdojo/campfireStories/services/UserService.java
@@ -2,9 +2,12 @@ package com.codingdojo.campfireStories.services;
 
 import java.util.List;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.Errors;
 
+import com.codingdojo.campfireStories.models.LoginUser;
 import com.codingdojo.campfireStories.models.User;
 import com.codingdojo.campfireStories.respositories.UserRepository;
 
@@ -26,4 +29,38 @@ public class UserService {
 		return userRepo.findByEmail(email);
 	}
 	
+	//Reg, Validate and Authen
+	
+	public void validate(User newUser, Errors errors) {
+		if(!newUser.getPassword().equals(newUser.getConfirm())) {
+			errors.rejectValue("password", "missmatch", "Passwords don't match");
+		}
+		if(userRepo.findByEmail(newUser.getEmail()) != null) {
+			errors.rejectValue("email", "duplicate", "Email already exists");
+		}
+	}
+	
+	public User registerUser(User newUser) {
+		String hashedPass = BCrypt.hashpw(newUser.getPassword(), BCrypt.gensalt());
+		
+		newUser.setPassword(hashedPass);
+		
+		userRepo.save(newUser);
+		
+		return null;
+	}
+	
+	public boolean authenticate(LoginUser newLogin, Errors errors) {
+		User user = userRepo.findByEmail(newLogin.getEmail());
+		if(user==null) {
+			errors.rejectValue("email", "not found", "Email not found");
+			return false;
+		} else {
+			if(!BCrypt.checkpw(newLogin.getPassword(), user.getPassword())) {
+				errors.rejectValue("password", "matches", "Invalid Password");
+				return false;
+			}
+		}
+		return true;
+	}	
 }
